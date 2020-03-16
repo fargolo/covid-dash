c900e33328398fb540cbf17b3167c5374c2dd13f
fargoo <felipe.c.argolo@protonmail.com>
2020-03-16 19:19:28 +0000

    wget in data-wrang

--- data/data-wrang.R
+++ data/data-wrang.R
@@ -1,6 +1,8 @@
 library(magrittr)
 library(tidyr)
 library(dplyr)
+#setwd("~/covid-dash/data")
+system("wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
 
 covid_df <- read.csv("time_series_19-covid-Confirmed.csv")
 covid_long <- covid_df %>% gather(var,val,-c(1:4))
